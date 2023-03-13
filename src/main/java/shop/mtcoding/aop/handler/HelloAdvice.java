0391df5b505d5091766da682027149bec05675e4
hoseok <shsshs0125@naver.com>
2023-03-13 14:56:08 +0900

    AOP로 매개변수 분석 완료

--- src/main/java/shop/mtcoding/aop/handler/HelloAdvice.java
+++ src/main/java/shop/mtcoding/aop/handler/HelloAdvice.java
@@ -1,8 +1,8 @@
 package shop.mtcoding.aop.handler;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +15,35 @@ public class HelloAdvice {
      */
     @Pointcut("@annotation(shop.mtcoding.aop.handler.aop.Hello)")
     public void hello() {
-
     }
 
-    @Before("hello()")
-    public void helloAdvice() {
-        System.out.println("안녕");
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void getMapping() {
     }
+
+    // @Before("hello()")
+    // public void helloAdvice() {
+    // System.out.println("안녕");
+    // }
+
+    // @After("getMapping() || hello()")
+    // public void getAdvice() {
+    // System.out.println("getMapping 실행됨");
+    // }
+
+    @Around("hello()")
+    public Object helloAdvice(ProceedingJoinPoint jp) throws Throwable { // ProceedingJoinPoint는 메서드 정보를 갖고 있다
+        Object[] args = jp.getArgs();
+        System.out.println("Parameter size: " + args.length);
+
+        for (Object arg : args) {
+            if (arg instanceof String) {
+                String username = (String) arg;
+                System.out.println(username + "님 안녕하세요");
+            }
+        }
+
+        return jp.proceed();
+    }
+
 }
